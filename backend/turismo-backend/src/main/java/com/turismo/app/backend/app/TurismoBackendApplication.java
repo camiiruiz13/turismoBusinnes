213b888e6aa02813f98a42c60f136fae372dbc2f
Camilo Ruiz <crcamiruiz13@gmail.com>
2025-03-16 17:18:08 -0500

    Se crea proyecto backend

---
+++ backend/turismo-backend/src/main/java/com/turismo/app/backend/app/TurismoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.turismo.app.backend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurismoBackendApplication.class, args);
+    }
+
+}
---
+++ backend/turismo-backend/src/test/java/com/turismo/app/backend/app/TurismoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turismo.app.backend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
